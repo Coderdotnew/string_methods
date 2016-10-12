6c4eaa04ac6f5c97386d419f9d4fefcad5a2b86d
Stephanholly <sholly@usc.edu>
2016-10-12 12:14:15 -0700

    first commit

---
+++ string_methods.swift
@@ -0,0 +1,11 @@
+// for each of the following lines of code, call a string method on the string to meet the criteria
+
+// print the following string in all capital letters: "Swift is the best language!"
+
+// print the following string in all lowercase letters: "HEY LOOK AT ME. I'M THE CAPTAIN NOW"
+
+// print the following string in reverse: "Albert Einstein"
+
+// add another string to the end of the string: "Hi! My name is "
+
+// Let's now add another string to the string you just created! 
